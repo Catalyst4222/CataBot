c559ee080489b5bcf5983b1f05806148c3c09721
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2021-07-12 15:57:30 -0400

    Adding a timestamp to the logger

--- catabot.py
+++ catabot.py
@@ -13,7 +13,7 @@ import logging
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-formatter = logging.Formatter('[%(levelname)s] [%(name)s]: %(message)s')
+formatter = logging.Formatter('[%(levelname)s] [%(asctime)s] [%(name)s]: %(message)s', '%H:%M:%S')
 
 file_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w+')
 file_handler.setLevel(logging.INFO)
