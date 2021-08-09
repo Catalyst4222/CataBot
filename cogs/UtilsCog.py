d6475779b8da18e0cfc03fd931f391150279aa85
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2021-08-08 22:45:36 -0400

    purge!: Remove the `create_roles` command BREAKING CHANGE: Whoop de dang do, a thing disappeared

--- cogs/UtilsCog.py
+++ cogs/UtilsCog.py
@@ -18,27 +18,6 @@ class UtilsCog(commands.Cog):
         #  if message.author == self.bot.user]
         await ctx.channel.purge(limit=messages+1, check=lambda x: x.author == self.bot.user)
 
-    @commands.command()
-    @commands.bot_has_permissions(manage_roles=True)
-    @commands.has_permissions(manage_roles=True)
-    async def create_roles(self, ctx: commands.Context, *, roles: typing.Optional[str] = None):
-        """Show/Generate different roles used by the bot"""
-        if roles is None:
-            return await ctx.send("""Roles and keywords:
-            \rGender roles: genders
-            \rMute role: mute     
-            """)
-
-        if roles == 'muted':
-            muted = await ctx.guild.create_role(name="Muted")
-            for channel in ctx.guild.channels:
-                await channel.set_permissions(muted, speak=False, send_messages=False)
-
-        elif roles == 'genders':
-            [
-                await ctx.guild.create_role(name=name)
-                for name in ['he/him', 'she/her', 'they/them']
-            ]
 
     @commands.command()
     async def invite(self, ctx, perms: typing.Optional[int] = 2483416129, slashCommands: typing.Optional[bool] = True):
