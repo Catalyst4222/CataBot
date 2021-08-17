bd19a5c44707d26f846f73bb30cb52eb84435723
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2021-08-17 18:04:06 -0400

    feat: add WhoIs user menu

--- cogs/UtilsCog.py
+++ cogs/UtilsCog.py
@@ -1,6 +1,10 @@
 import typing
 
+import discord
 from discord.ext import commands
+from discord_slash.model import ContextMenuType
+from discord_slash.cog_ext import cog_context_menu
+from discord_slash.context import MenuContext
 
 
 class UtilsCog(commands.Cog):
@@ -34,6 +38,42 @@ class UtilsCog(commands.Cog):
         if slashCommands:
             msg += '&scope=bot%20applications.commands'
         return await ctx.send(msg)
+
+    # Menu, usertype
+    # whois
+    # stealing dyno's thing
+
+    # user.name + user.discriminator?
+    # Joined: .target_user.joined_at
+    # Registered: .target_user.created_at
+    # Roles: len(.target_user.roles)-1, [role.mention for role in .target_user.roles]
+
+    # color: .target_user.color
+    # image/thumbnail: .target_user.default_avatar_url
+    # footer: member.id
+
+    @cog_context_menu(name='WhoIs', target=ContextMenuType.USER,)
+    async def whois(self, ctx: MenuContext):
+        member: discord.Member = ctx.target_author
+        form = '%B %d, %Y'
+        # await ctx.send('This is currently in testing', hidden=True)
+
+        embed = discord.Embed(color=member.color) \
+            .set_author(name=str(member), icon_url=member.avatar_url) \
+            .set_thumbnail(url=member.avatar_url) \
+            .set_footer(text=f'ID: {member.id}')
+
+        embed.add_field(name='Joined:',
+                        value=member.joined_at.__format__(form)) \
+             .add_field(name='Registered:',
+                        value=member.created_at.date().__format__(form)) \
+             .add_field(name=f'Roles: {len(member.roles)-1}',
+                        value=' '.join(
+                            [role.mention for role in member.roles][1:]
+                        ),
+                        inline=False)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
